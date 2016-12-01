a065dbf72d320f3c4660c1095792c8579e57fcd6
pts1989 <pts1989@gmail.com>
2016-12-01 12:16:09 +0100

    update

--- WebApiTester/WebApiTester/index.php
+++ WebApiTester/WebApiTester/index.php
@@ -5,12 +5,12 @@ $klantNummer = "8700";
 date_default_timezone_set('UTC');
 $datum = date_format(date_create(),"d-m-y G:i:s.u");
 $baseString = sprintf("%s|%s|%s", substr($datum, 0, 8),substr($datum, 9, 12), $klantNummer);
-
+echo "test";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,"https://acceptatie.sdbstart.nl/Start/api/dienstverbanden");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+echo "test2";
 $headers = [
     'Cache-Control: no-cache',
     'Content-Type: application/xml; charset=utf-8',
@@ -20,11 +20,11 @@ $headers = [
 ];
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
+echo "test3";
 $server_output = curl_exec ($ch);
-
+echo "test4";
 curl_close ($ch);
-
-print  $server_output ;
+echo "test5";
+//print  $server_output ;
 
 ?>

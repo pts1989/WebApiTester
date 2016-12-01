7f7a93e89c6747860ab562e850fa9599d8dd4a1e
pts1989 <pts1989@gmail.com>
2016-12-01 14:42:08 +0100

    final

--- WebApiTester/WebApiTester/index.php
+++ WebApiTester/WebApiTester/index.php
@@ -1,43 +1,25 @@
 <?php
-error_reporting(E_ALL);
-$apiUser = "TEST";
-$apiSecret = "testsleutel";
-$klantNummer = "8700";
-date_default_timezone_set('UTC');
-$datum = date_format(date_create(),"d-m-y G:i:s.u");
-$baseString = sprintf("%s|%s|%s", substr($datum, 0, 8),substr($datum, 9, 12), $klantNummer);
-$hmac = base64_encode(hash_hmac("sha256",$baseString,$apiSecret,true));
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://acceptatie.sdbstart.nl/Start/api/dienstverbanden");
-curl_setopt($ch, CURLOPT_HTTPGET, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+    date_default_timezone_set('UTC');
+    $curl = curl_init();
+    $apiUser = "TEST";
+    $apiSecret = "testsleutel";
+    $klantNummer = "8700";
+    $datum = date_format(date_create(), "d-m-Y G:i:s.u");
+    $baseString = sprintf("%s|%s|%s", substr($datum, 0, 10), substr($datum, 11, 12), $klantNummer);
+    $headers = [
+        'Content-Type: application/xml',
+        'Accept: application/xml',
+        'Timestamp: '.substr($datum, 0, 23),
+        'Klantnummer: '.$klantNummer,
+	    'Authentication: '.sprintf("%s:%s", $apiUser, base64_encode(hash_hmac("sha256", $baseString, strtoupper($apiSecret), true))),
+    ];
 
-$headers = [
-    'Cache-Control: no-cache',
-    'Content-Type: application/xml; charset=utf-8',
-    'Timestamp: '.$datum,
-    'Klantnummer: '.$klantNummer,
-	'Authentication: '.sprintf("%s:%s", $apiUser,$hmac)
-];
+    curl_setopt($curl, CURLOPT_URL, "https://acceptatie.sdbstart.nl/Start/api/dienstverbanden");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-
-if( ! $result = curl_exec($ch))
-{
-    trigger_error(curl_error($ch));
-}
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-print_r( $result);
-$server_output = curl_exec ($ch);
-
-curl_close ($ch);
-
-
-print_r($server_output);
-print_r($headers);
-print_r($httpCode);
-echo"final";
-echo $hmac;
+    $server_output = curl_exec($curl);
+    curl_close ($curl);
+    print_r($server_output);
 ?>
